ccb6a558421721b66ea04381ce863dadab7d04f2
richard569-oss <richardweizeus@gmail.com>
2026-04-20 14:34:03 -0400

    fix puerto render

--- app.py
+++ app.py
@@ -21,5 +21,8 @@ def contacto():
 
     return render_template('contacto.html')
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
